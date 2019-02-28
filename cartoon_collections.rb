dee82decbf940b85a310b230ba6747bb2d8df2a5
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-28 13:01:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ end
 def summon_captain_planet(planeteer_calls)
 planeteer_calls.map do "{|call| call.capiltalize + "!"}"
 end
+summon_captain_planet
 
 def long_planeteer_calls# code an argument here
   # Your code here
