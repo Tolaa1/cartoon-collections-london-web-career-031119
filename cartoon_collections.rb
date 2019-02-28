0d5ff39c26c01b295907ac8445e44368bdb529c2
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-28 13:08:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index+1},#{dwarf}"
 end
+roll_call_dwarves
 
 def summon_captain_planet(planeteer_calls)
 planeteer_calls.map do "{|call| call.capiltalize + "!"}"
@@ -11,6 +12,7 @@ summon_captain_planet
 def long_planeteer_calls(calls)
   long_planeteer_calls.any? do |call| call.length>4|
 end
+long_planeteer_calls
 
 def find_the_chees(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
